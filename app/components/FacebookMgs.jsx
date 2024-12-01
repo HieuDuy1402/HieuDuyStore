84c8424b903c64f015626ddb58e80a0d9c879669
HieuDuy1402 <duynguyenhieuduc@gmail.com>
2024-12-02 03:42:01 +0700

    commit

--- app/components/FacebookMgs.jsx
+++ app/components/FacebookMgs.jsx
@@ -6,7 +6,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookMgs = () => {
   return (
     <FacebookProvider appId="9362543673759105" chatSupport>
-      <CustomChat pageId="455773877626994" minimized={true} />
+      <CustomChat pageId="455773877626994" minimized="true" />
     </FacebookProvider>
   );
 };
