240f69a6b8353cd687e5b6cafb277d7f99763838
HieuDuy1402 <duynguyenhieuduc@gmail.com>
2024-12-02 03:01:32 +0700

    update

--- app/components/FacebookMgs.jsx
+++ app/components/FacebookMgs.jsx
@@ -3,10 +3,12 @@
 import React from "react";
 import { FacebookProvider, CustomChat } from "react-facebook";
 
-export default function FacebookMgs () {
+const FacebookMgs = () => {
   return (
     <FacebookProvider appId="9362543673759105" chatSupport>
       <CustomChat pageId="455773877626994" minimized={true} />
     </FacebookProvider>
   );
 };
+
+export default FacebookMgs;
